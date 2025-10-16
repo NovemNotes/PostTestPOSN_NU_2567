7c1bb2f9de6129cb654e5f8e9afaa9b44f96d203
Nithit Sitthirat <87806999+NovemNotes@users.noreply.github.com>
2025-10-16 14:07:46 +0700

    Update VillageBoxingMatch.cpp

--- VillageBoxingMatch.cpp
+++ VillageBoxingMatch.cpp
@@ -1,31 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n,ans = 0,k=1;
-vector<pair<int,int>> v;
-
-int check(int i,int j){
-    int m = v[i].first * v[j].first;
-    int num = k;
-    if(v[i].second != 0)num /= v[i].second;
-    if(v[j].second != 0)num /= v[j].second;
-    
-    return m * num;
-}
-
 int main(){
-    cin.tie(nullptr)->sync_with_stdio(false);
-    cin >> n;
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+    int n;cin >> n;
+    vector<pair<int,int>> v(n);
+    int sum=0;
     for(int i=0;i<n;i++){
-        int x,y;cin >> x >> y;
-        v.push_back({x,y});
-        if(y!=0)k*=y;
+        cin >> v[i].first >> v[i].second;
+        sum+=v[i].second;
     }
+    int ans=0;
     for(int i=0;i<n;i++){
         for(int j=i+1;j<n;j++){
-            ans += check(i,j);
+            int pe = sum-v[i].second-v[j].second;
+            int box = v[i].first*v[j].first;
+            ans+=pe*box;
         }
     }
-    cout << ans;
+    cout << ans << "\n";
     return 0;
 }
